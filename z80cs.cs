ac5749208e17f177b1a15d223035b6290a09aa6d
scudo005 <96631443+scudo005@users.noreply.github.com>
2025-07-29 23:26:10 +0200

    stuff + added docs

--- z80cs.cs
+++ z80cs.cs
@@ -5,9 +5,10 @@ namespace z80cs
 {
     internal class Z80_CPU
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            _ = new Z80_CPU();
+            // we need to init memory...
         }
 
         private byte AReg,
@@ -29,7 +30,9 @@ namespace z80cs
             MemRefreshReg,
             InterruptVectorReg,
             IODeviceAddress,
-            IMF; // this register takes care of managing interrupt modes and must be managed with its 2 lower bits
+            IMF;
+
+        /// this register takes care of managing interrupt modes and must be managed with its 2 lower bits
         private ushort ProgCounterReg,
             IXIndexReg,
             IYIndexReg,
@@ -49,6 +52,9 @@ namespace z80cs
         private byte[] AddressSpace = new byte[0xFFFF]; // this always reserves 64k of RAM. lol
         readonly CultureInfo ci = new("en-us");
 
+        /// <summary>
+        /// Creates a new CPU object instance and starts it.
+        /// </summary>
         public Z80_CPU()
         {
             Reset();
@@ -64,6 +70,9 @@ namespace z80cs
             }
         }
 
+        /// <summary>
+        /// Initializes all of the registers and clears RAM.
+        /// </summary>
         public void Reset()
         {
             AReg = 0;
@@ -100,26 +109,54 @@ namespace z80cs
             IFF1 = true; // maskable interrupts are enabled by default
             IFF2 = false;
             IMF = 0; // mode 0 because I say so
+            for (int i = 0; i < 0xFFFF; i++)
+            {
+                AddressSpace[i] = (byte)(0x02);
+            }
         }
 
         // these update methods must be called every time A, F, H, L, D, E, B, C registers are used to not desync the state of AF, HL, DE, BC and vice versa
         // TODO: this machine is little endian, invert bytes
 
-        private void UpdatePairedRegFrom8Bit(ushort registerPair, byte upperReg, byte lowerReg) // this has correct endianness
+        /// <summary>
+        /// Updates the value of the paired register based on the status of the two register that make it up.
+        /// <param name="registerPair">The register pair to be updated.</param>
+        /// <param name="upperReg">The 8-bit register that occupies the upper 8 bits of our register pair.</param>
+        /// <param name="lowerReg">The 8-bit register that occupies the lower 8 bits of our register pair.</param>
+        /// </summary>
+        /// <remarks>Use only valid register pairs (AF, BC, DE, HL).</remarks>
+        private void UpdatePairedRegFrom8Bit(ushort registerPair, byte upperReg, byte lowerReg)
         {
-            registerPair = (ushort)(upperReg); // the bits of the upper Register go into the register pair
+            registerPair = (ushort)(upperReg); // the bits of the upper register go into the register pair
             registerPair = (ushort)(registerPair << 8); // the 8 bits of the upper register are shifted into the upper 16 bits of the register pair
             registerPair = (ushort)(registerPair + lowerReg); // and now we put in the contents of the lower register
         }
 
+        /// <summary>
+        /// Updates the value of the two 8 bit registers that make up a register pair.
+        /// <param name="registerPair">The register pair to be updated.</param>
+        /// <param name="upperReg">The 8-bit register that occupies the upper 8 bits of our register pair.</param>
+        /// <param name="lowerReg">The 8-bit register that occupies the lower 8 bits of our register pair.</param>
+        /// </summary>
+        /// <remarks>Use only valid register pairs (AF, BC, DE, HL).</remarks>
         private void UpdateUnpairedRegFrom16Bit(ushort registerPair, byte upperReg, byte lowerReg)
         {
             lowerReg = (byte)(registerPair); // we simply discard the upper 16 bits
             upperReg = (byte)(registerPair >> 8); // the upper register is stored in the upper 8 bits of the register pair
         }
 
+        /// <summary>
+        /// Automatically fetches and executes the next instruction found in the address space.
+        /// </summary>
         private void NextInstruction()
         {
+            if ((ProgCounterReg + 1) > 0xFFFF)
+            {
+                Console.WriteLine("overflow pc");
+                ProgCounterReg = 0x0000;
+                // Environment.Exit(-1);
+            }
+
             byte opcode = AddressSpace[ProgCounterReg];
             switch (opcode)
             {
@@ -130,28 +167,28 @@ namespace z80cs
                     ld16bc(FetchData16Bit());
                     break;
                 case 0x02:
-                    ldARegToBCPointer(FetchData16Bit());
+                    LdARegToBCPointer(FetchData16Bit());
                     break;
                 case 0x03:
-                    incBCReg();
+                    IncBCReg();
                     break;
                 case 0x04:
-                    incBReg();
+                    IncBReg();
                     break;
                 case 0x05:
-                    decBReg();
+                    DecBReg();
                     break;
                 case 0x06:
-                    ldBRegConst(FetchData8Bit());
+                    LdBRegConst(FetchData8Bit());
                     break;
                 case 0x07:
-                    rlca();
+                    Rlca();
                     break;
                 case 0x08:
-                    exchangeAFWithAFSec();
+                    ExchangeAFWithAFSec();
                     break;
                 case 0x09:
-                    addHLBC();
+                    AddHLBC();
                     break;
                 default:
                     Console.WriteLine(
@@ -159,15 +196,24 @@ namespace z80cs
                         opcode.ToString("X", ci),
                         ProgCounterReg.ToString("X", ci)
                     );
+                    ProgCounterReg++; // for now, ignore unknown opcodes like 3DS Virtual Console. Maybe call halt instruction when implemented.
                     break;
             }
         }
 
+        /// <summary>
+        /// Fetches the next byte from the address space to be used as data.
+        /// <returns>A byte of data.</returns>
+        /// </summary>
         private byte FetchData8Bit()
         {
             return AddressSpace[ProgCounterReg++];
         }
 
+        /// <summary>
+        /// Fetches the next 2 bytes from the address space to be used as data.
+        /// <returns>2 bytes of data packed as a 16 bit value.</returns>
+        /// </summary>
         private ushort FetchData16Bit()
         {
             ushort ret = AddressSpace[ProgCounterReg++];
@@ -176,34 +222,62 @@ namespace z80cs
             return ret;
         }
 
+        /// Here start the instructions' implementations. Note that writing to the console is essential to the program not running too fast and overflowing PC immediately.
+
+        /// <summary>
+        /// No operation, only increases PC.
+        /// </summary>
         private void nop()
         {
+            Console.WriteLine("nop");
             ProgCounterReg++;
+            RegisterStatus();
         }
 
+        /// <summary>
+        /// Loads a 16 bit value into register BC.
+        /// <param name="operand">The 16 bit value to be loaded.</param>
+        /// </summary>
         private void ld16bc(ushort operand)
         {
+            Console.WriteLine("ld bc, " + operand);
             BCReg = operand;
             UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg); // B and C have synced contents
             ProgCounterReg++;
         }
 
-        private void ldARegToBCPointer(ushort operand)
+        /// <summary>
+        /// Loads an 8 bit value into register A, found at the location pointed by the operand.
+        /// <param name="operand">The 16 bit pointer to the desidered value.</param>
+        /// </summary>
+        private void LdARegToBCPointer(ushort operand)
         {
-            AddressSpace[BCReg] = AReg;
+            Console.WriteLine("ld a, [" + operand + "]");
+            AddressSpace[operand] = AReg;
+            Console.WriteLine("[bc] is now " + AddressSpace[BCReg] + " with bc = " + BCReg);
             UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg);
             ProgCounterReg++;
+            RegisterStatus();
         }
 
-        private void incBCReg() // we don't check for carry here: the CPU couldn't care less.
+        /// <summary>
+        /// Increases the value of register BC by 1.
+        /// </summary>
+        private void IncBCReg() // we don't check for carry here: the CPU couldn't care less.
         {
+            Console.WriteLine("inc bc");
             BCReg++;
             UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg);
             ProgCounterReg++;
+            RegisterStatus();
         }
 
-        private void incBReg()
+        /// <summary>
+        /// Increases the value of register B by 1.
+        /// </summary>
+        private void IncBReg()
         {
+            Console.WriteLine("inc b");
             if ((BReg + 1) > 0xFF)
             {
                 BReg = 0;
@@ -224,10 +298,15 @@ namespace z80cs
                 UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
                 ProgCounterReg++;
             }
+            RegisterStatus();
         }
 
-        private void decBReg()
+        /// <summary>
+        /// Decreases the value of register B by 1.
+        /// </summary>
+        private void DecBReg()
         {
+            Console.WriteLine("dec b");
             if ((BReg - 1) > 0xFF)
             {
                 BReg = 0;
@@ -248,21 +327,32 @@ namespace z80cs
                 UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
                 ProgCounterReg++;
             }
+            RegisterStatus();
         }
 
-        private void ldBRegConst(byte operand)
+        /// <summary>
+        /// Loads an 8 bit value into register B.
+        /// <param name="operand">The 8 bit value to be loaded.</param>
+        /// </summary>
+        private void LdBRegConst(byte operand)
         {
+            Console.WriteLine("ld b, " + operand);
             BReg = operand;
             UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
             ProgCounterReg++;
+            RegisterStatus();
         }
 
-        private void rlca()
-        { // rotate left A Register contents by one bit
+        /// <summary>
+        /// Rotates left A register contents by one bit.
+        /// <remark>https://stackoverflow.com/questions/4439078/how-do-you-set-only-certain-bits-of-a-byte-in-c-without-affecting-the-rest#4439221</remark>
+        /// </summary>
+        private void Rlca()
+        {
+            Console.WriteLine("rlca");
             AReg = (byte)(AReg << 1);
             SetHalfCarryState(0x00);
             SetAddSubFlagState(0x00);
-            // https://stackoverflow.com/questions/4439078/how-do-you-set-only-certain-bits-of-a-byte-in-c-without-affecting-the-rest#4439221
             if ((AReg & (1 << 0x80)) != 0) // we take the MSB/rightmost bit of the A register and we copy its value to the carry flag.
             {
                 SetCarryFlagState(0x01);
@@ -275,18 +365,29 @@ namespace z80cs
                 // AReg = (byte)(((byte)(AReg) & ~(byte)(0x01)) | (0x00 & (byte)(0x01)));
             }
             ProgCounterReg++;
+            RegisterStatus();
         }
 
-        private void exchangeAFWithAFSec()
+        /// <summary>
+        /// Exchanges the values of the AF and AF' registers.
+        /// </summary>
+        private void ExchangeAFWithAFSec()
         {
+            Console.WriteLine("ex af, af'");
             ushort AFCopy = AFReg;
             AFReg = AFRegSec;
             AFRegSec = AFCopy;
+            // (AFRegSec, AFReg) = (AFReg, AFRegSec); // VS Code suggests we do this: I say it's incomprehensible.
             ProgCounterReg++;
+            RegisterStatus();
         }
 
-        private void addHLBC()
+        /// <summary>
+        /// Adds the contents of the BC register to the contents of the HL register.
+        /// </summary>
+        private void AddHLBC()
         {
+            Console.WriteLine("add hl, bc");
             SetAddSubFlagState(0x00);
             if ((HLReg + BCReg) > 0xFFFF) // this gets valued as an int and not as an ushort, because C#.
             {
@@ -299,6 +400,7 @@ namespace z80cs
             }
 
             ProgCounterReg++;
+            RegisterStatus();
         }
 
         // these functions should get a status byte of which the LSB/leftmost bit needs to be set to enable the flag and unset to disable it.
@@ -342,6 +444,31 @@ namespace z80cs
         private void ManageNMI()
         {
             ProgCounterReg = NMIVector;
+        }
+
+        private void RegisterStatus()
+        {
+            Console.Write("a: " + AReg);
+            Console.Write(" b: " + BReg);
+            Console.Write(" c: " + CReg);
+            Console.Write(" d: " + DReg);
+            Console.Write(" e: " + EReg);
+            Console.Write(" f: " + FReg);
+            Console.Write(" h: " + HReg);
+            Console.WriteLine(" l: " + LReg);
+            Console.Write("af: " + AFReg);
+            Console.Write(" bc: " + BCReg);
+            Console.Write(" de: " + DEReg);
+            Console.WriteLine(" hl: " + HLReg);
+            /*Console.Write("a': " + ARegSec);
+            Console.Write(" b': " + BRegSec);
+            Console.Write(" c': " + CRegSec);
+            Console.Write(" d': " + DRegSec);
+            Console.Write(" e': " + ERegSec);
+            Console.Write(" f': " + FRegSec);
+            Console.Write(" h': " + HRegSec);
+            Console.WriteLine(" l': " + LRegSec);*/
+            Console.WriteLine("pc: " + ProgCounterReg);
         }
     }
 }
