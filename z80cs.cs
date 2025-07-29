1876ab1e811d4efde66fa4bedc633a1fac66aac5
scudo005 <96631443+scudo005@users.noreply.github.com>
2025-07-29 23:26:09 +0200

    added missing register copies

--- z80cs.cs
+++ z80cs.cs
@@ -18,6 +18,14 @@ namespace z80cs
             FReg,
             HReg,
             LReg,
+            ARegSec,
+            BRegSec,
+            CRegSec,
+            DRegSec,
+            ERegSec,
+            FRegSec,
+            HRegSec,
+            LRegSec,
             MemRefreshReg,
             InterruptVectorReg,
             IODeviceAddress,
@@ -26,9 +34,14 @@ namespace z80cs
             IXIndexReg,
             IYIndexReg,
             StackPointerReg,
+            AFReg,
             HLReg,
             BCReg,
-            DEReg;
+            DEReg,
+            AFRegSec,
+            HLRegSec,
+            BCRegSec,
+            DERegSec;
         private Boolean NMITrigger,
             IFF1, // IFF1 is for disabling maskable interrupts: if it's 1, IRQs are enabled.
             IFF2;
@@ -51,9 +64,22 @@ namespace z80cs
             FReg = 0;
             HReg = 0;
             LReg = 0;
-            UpdateHLFrom8BitReg();
-            UpdateBCFrom8BitReg();
-            UpdateDEFrom8BitReg();
+            ARegSec = 0;
+            BRegSec = 0;
+            CRegSec = 0;
+            DRegSec = 0;
+            ERegSec = 0;
+            FRegSec = 0;
+            HRegSec = 0;
+            LRegSec = 0;
+            UpdatePairedRegFrom8Bit(AFReg, AReg, FReg);
+            UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
+            UpdatePairedRegFrom8Bit(DEReg, DReg, EReg);
+            UpdatePairedRegFrom8Bit(HLReg, HReg, LReg);
+            UpdatePairedRegFrom8Bit(AFRegSec, ARegSec, FRegSec);
+            UpdatePairedRegFrom8Bit(BCRegSec, BRegSec, CRegSec);
+            UpdatePairedRegFrom8Bit(DERegSec, DRegSec, ERegSec);
+            UpdatePairedRegFrom8Bit(HLRegSec, HRegSec, LRegSec);
             MemRefreshReg = 0;
             InterruptVectorReg = 0;
             ProgCounterReg = 0x0000; // verified good
@@ -66,45 +92,20 @@ namespace z80cs
             IMF = 0; // mode 0 because I say so
         }
 
-        // these update methods must be called every time H, L, D, E, B, C registers are used to not desync the state of HL, DE, BC
+        // these update methods must be called every time A, F, H, L, D, E, B, C registers are used to not desync the state of AF, HL, DE, BC and vice versa
         // TODO: is this machine big endian or little endian?
-        private void UpdateHLFrom8BitReg()
+
+        private void UpdatePairedRegFrom8Bit(ushort registerPair, byte upperReg, byte lowerReg)
         {
-            HLReg = (ushort)(HReg); // the bits of the H Register go into HL
-            HLReg = (ushort)(HLReg << 8); // the 8 bits of the H register are shifted into the upper 16 bits of the HL Register
-            HLReg = (ushort)(HLReg + LReg); // and now we put in the contents of the L Register
+            registerPair = (ushort)(upperReg); // the bits of the upper Register go into the register pair
+            registerPair = (ushort)(registerPair << 8); // the 8 bits of the upper register are shifted into the upper 16 bits of the register pair
+            registerPair = (ushort)(registerPair + lowerReg); // and now we put in the contents of the lower register
         }
 
-        private void UpdateBCFrom8BitReg()
+        private void UpdateUnpairedRegFrom16Bit(ushort registerPair, byte upperReg, byte lowerReg)
         {
-            BCReg = (ushort)(BReg);
-            BCReg = (ushort)(BCReg << 8);
-            BCReg = (ushort)(BCReg + CReg);
-        }
-
-        private void UpdateDEFrom8BitReg()
-        {
-            DEReg = (ushort)(DReg);
-            DEReg = (ushort)(DEReg << 8);
-            DEReg = (ushort)(DEReg + EReg);
-        }
-
-        private void UpdateHLFrom16BitReg()
-        {
-            LReg = (byte)(HLReg); // we discard the upper 16 bits
-            HReg = (byte)(HLReg >> 8); // the H register is stored in the upper 8 bits of the HL Register
-        }
-
-        private void UpdateDEFrom16BitReg()
-        {
-            EReg = (byte)(DEReg);
-            DReg = (byte)(DEReg >> 8);
-        }
-
-        private void UpdateBCFrom16BitReg()
-        {
-            CReg = (byte)(BCReg);
-            BReg = (byte)(BCReg >> 8);
+            lowerReg = (byte)(registerPair); // we simply discard the upper 16 bits
+            upperReg = (byte)(registerPair >> 8); // the upper register is stored in the upper 8 bits of the register pair
         }
 
         private void NextInstruction()
@@ -127,6 +128,15 @@ namespace z80cs
                 case 0x04:
                     incBReg();
                     break;
+                case 0x05:
+                    decBReg();
+                    break;
+                case 0x06:
+                    ldBRegConst(FetchData8Bit());
+                    break;
+                case 0x07:
+                    rlca();
+                    break;
                 default:
                     Console.WriteLine(
                         "Unknown opcode 0x{0} occurred at: 0x{1}",
@@ -145,7 +155,7 @@ namespace z80cs
         private ushort FetchData16Bit()
         {
             ushort ret = AddressSpace[ProgCounterReg++];
-            ret = (ushort)(ret << 8); // we shift the first 8 bits to the right
+            ret = (ushort)(ret << 8); // we shift the first 8 bits to the right // can't we just say ret << 8 instead of reassigning the value?
             ret = AddressSpace[ProgCounterReg++]; // this fits into the lower 8 bits
             return ret;
         }
@@ -158,21 +168,21 @@ namespace z80cs
         private void ld16bc(ushort operand)
         {
             BCReg = operand;
-            UpdateBCFrom16BitReg(); // B and C have synced contents
+            UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg); // B and C have synced contents
             ProgCounterReg++;
         }
 
         private void ldARegToBCPointer(ushort operand)
         {
             AddressSpace[BCReg] = AReg;
-            UpdateBCFrom16BitReg();
+            UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg);
             ProgCounterReg++;
         }
 
         private void incBCReg() // we don't check for carry here: the CPU couldn't care less.
         {
             BCReg++;
-            UpdateBCFrom16BitReg();
+            UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg);
             ProgCounterReg++;
         }
 
@@ -182,24 +192,81 @@ namespace z80cs
             {
                 BReg = 0;
                 SetParityOverfState(0x01); // note to self: remind to take care of the carry.
-                UpdateBCFrom8BitReg();
+                UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
+                ProgCounterReg++;
+            }
+            else if ((BReg + 1) == 0)
+            {
+                BReg = 0;
+                SetZeroFlagState(0x01);
+                UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg);
                 ProgCounterReg++;
             }
             else
             {
                 BReg++;
-                UpdateBCFrom8BitReg();
+                UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
                 ProgCounterReg++;
             }
         }
 
+        private void decBReg()
+        {
+            if ((BReg - 1) > 0xFF)
+            {
+                BReg = 0;
+                SetParityOverfState(0x01); // yes
+                UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
+                ProgCounterReg++;
+            }
+            else if ((BReg - 1) == 0)
+            {
+                BReg = 0;
+                SetZeroFlagState(0x01);
+                UpdateUnpairedRegFrom16Bit(BCReg, BReg, CReg);
+                ProgCounterReg++;
+            }
+            else
+            {
+                BReg--;
+                UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
+                ProgCounterReg++;
+            }
+        }
+
+        private void ldBRegConst(byte operand)
+        {
+            BReg = operand;
+            UpdatePairedRegFrom8Bit(BCReg, BReg, CReg);
+            ProgCounterReg++;
+        }
+
+        private void rlca()
+        { // rotate left A Register contents by one bit
+            AReg = (byte)(AReg << 1);
+            SetHalfCarryState(0x00);
+            SetAddSubFlagState(0x00);
+            // https://stackoverflow.com/questions/4439078/how-do-you-set-only-certain-bits-of-a-byte-in-c-without-affecting-the-rest#4439221
+            if ((AReg & (1 << 0x80)) != 0) // we take the MSB/rightmost bit of the A register and we copy its value to the carry flag.
+            {
+                SetCarryFlagState(0x01);
+                // AReg = (byte)(((byte)(AReg) & ~(byte)(0x01)) | (0x01 & (byte)(0x01))); // we set bit 0 corresponding to bit 7's value.
+                // didn't the leftshift operation already do this?
+            }
+            else
+            {
+                SetCarryFlagState(0x00);
+                // AReg = (byte)(((byte)(AReg) & ~(byte)(0x01)) | (0x00 & (byte)(0x01)));
+            }
+            ProgCounterReg++;
+        }
+
         // these functions should get a status byte of which the LSB/leftmost bit needs to be set to enable the flag and unset to disable it.
-        // also these random ass 0x?? bytes
+        // also these random ass 0x?? bytes represent the bitmask of the several set flags
         private void SetCarryFlagState(byte status)
         {
             FReg = (byte)(((byte)(FReg) & ~(byte)(0x01)) | (status & (byte)(0x01))); // WTF??? In theory this should set our carry flag tho.
             // https://stackoverflow.com/questions/127027/how-can-i-check-my-byte-flag-verifying-that-a-specific-bit-is-at-1-or-0#127062
-            // https://stackoverflow.com/questions/4439078/how-do-you-set-only-certain-bits-of-a-byte-in-c-without-affecting-the-rest#4439221
             // I bet this is easier to do in silicon design.
         }
 
@@ -230,6 +297,11 @@ namespace z80cs
         private void SetSignFlagState(byte status)
         {
             FReg = (byte)(((byte)(FReg) & ~(byte)(0x80)) | (status & (byte)(0x80)));
+        }
+
+        private void ManageNMI()
+        {
+            ProgCounterReg = NMIVector;
         }
     }
 }
